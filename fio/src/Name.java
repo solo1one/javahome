bd6265ca04b0e49bd6d08ce07fb64c8090a05906
Shamil Mag <sha24013@gmail.com>
2019-12-24 21:14:07 +0300

    with string method

--- fio/src/Name.java
+++ fio/src/Name.java
@@ -1,28 +1,46 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Name {
     public static void main(String[] args) {
 
-        String fio ;
-        String name;
-        String lastName;
-        String midName;
+        String fio;
 
         System.out.println("Введите Фамилию Имя Отчество");
         Scanner sFio = new Scanner(System.in);
         fio = sFio.nextLine();
 
-        String[] words = fio.split("\\s");
-        lastName = words[0];
-        name = words[1];
-        midName = words[2];
+        if (fio.indexOf(" ") == fio.lastIndexOf(" "))
+        {
+            System.out.println("madness");
+        } else {
 
-        System.out.println("фамилия: " + lastName +'\n'+ "имя: " + name + '\n' + "отчество: " + midName );
+            String name = fio.substring(0, fio.indexOf(" "));
+            String midName = fio.substring(fio.indexOf(" "), fio.lastIndexOf(" "));
+            String lastName = fio.substring(fio.lastIndexOf(" "));
+            System.out.println(lastName);
+        }
 
 
+        /*if (nameVal(fio)){
+            String[] words = fio.split("\\s");
+            lastName = words[0];
+            name = words[1];
+            midName = words[2];
+            System.out.println("фамилия: " + lastName + '\n' + "имя: " + name + '\n' + "отчество: " + midName);
+        }
+        else {
+            System.out.println("Неверный ввод");
+        }
+    }
 
 
+    private static boolean nameVal(String fio){
+        Pattern validFio = Pattern.compile("[А-Яа-яA-Za-z]+'\\s' [А-Яа-яA-Za-z]+'\\s' [А-Яа-яA-Za-z]+");
+        Matcher trueFio = validFio.matcher(fio);
+        return trueFio.matches();*/
 
     }
 }
